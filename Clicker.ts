01b112e5c29e35a7209722cb416992309e0ae3b2
Lanshuye <927980270@qq.com>
2023-07-31 20:23:27 +0800

    Update Clicker.ts

--- Clicker.ts
+++ Clicker.ts
@@ -8,43 +8,38 @@ const ClickerSettings = {
 
 var Clicker = {} as any;
 
-Clicker.ClickerFunction = ((ev:MouseEvent|any)=>{
-
-    if(ev.path[0].localName != "div" && ev.path[0].localName != "body" && ev.path[0].localName != "html"){
+Clicker.ClickerFunction = (function (ev) {
+    if (ev.target.localName != "div" && ev.target.localName != "body" && ev.target.localName != "html") {
         return;
     }
-
     var GA = 1;
     var Text = Clicker.CountGroup[Clicker.Count];
-
     var Canvas = window.document.createElement("canvas");
     Canvas.style.backgroundColor = "transparent";
-    Canvas.width = 100;
-    Canvas.height = 100;
+    Canvas.width = 40;
+    Canvas.height = 25;
     var Context = Canvas.getContext("2d");
     Context.fillStyle = ClickerSettings.Color;
     Context.font = ClickerSettings.Font;
-    Context.fillText(Text,0,20);
+    Context.fillText(Text, 0, 20);
     window.document.body.append(Canvas);
     Canvas.style.position = "fixed";
-    Canvas.style.top = ev.y.toString();
-    Canvas.style.left = ev.x.toString();
-
+    Canvas.style.top = (ev.y - 12).toString();
+    Canvas.style.left = (ev.x - 20).toString();
     Clicker.Count = Clicker.Count + 1;
-    if(Clicker.Count == 12){
+    if (Clicker.Count == 12) {
         Clicker.Count = 0;
     }
-
-    setInterval(()=>{
-        Context.clearRect(0,0,100,100);
+    setInterval(function () {
+        Context.clearRect(0, 0, 100, 100);
+        Canvas.style.top = (Canvas.style.top - 12).toString();
         GA = GA - 0.01;
         Context.globalAlpha = GA;
-        Context.fillText(Text,0,20);
-    },ClickerSettings.Time / 100)
-
-    setTimeout(()=>{
-        Canvas.remove()
-    },ClickerSettings.Time)
+        Context.fillText(Text, 0, 20);
+    }, ClickerSettings.Time / 100);
+    setTimeout(function () {
+        Canvas.remove();
+    }, ClickerSettings.Time);
 });
 
 Clicker.Count = 0;
